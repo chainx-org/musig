48009fdfaf60e523d3196163b4f1f9f0cee5c30a
hacpy <hacpy.lbx@gmail.com>
2021-10-13 17:00:24 +0800

    Make mast static

--- musig/src/main/java/com/chainx/musig/Mast.java
+++ musig/src/main/java/com/chainx/musig/Mast.java
@@ -5,15 +5,15 @@ import android.text.TextUtils;
 import com.sun.jna.Native;
 
 public class Mast {
-    public String generateThresholdPubkey(String[] pubkeys, byte threshold) {
+    public static String generateThresholdPubkey(String[] pubkeys, byte threshold) {
         return clib.generate_threshold_pubkey(TextUtils.join("", pubkeys).toString(), threshold);
     }
 
-    public String generateControlBlock(String[] pubkeys, byte threshold, String sigAggPubkey) {
+    public static String generateControlBlock(String[] pubkeys, byte threshold, String sigAggPubkey) {
         return clib.generate_control_block(TextUtils.join("", pubkeys).toString(), threshold, sigAggPubkey);
     }
 
-    final CLibrary clib = (CLibrary) Native.load(
+    final static CLibrary clib = (CLibrary) Native.load(
             "musig_dll",
             CLibrary.class);
 }
